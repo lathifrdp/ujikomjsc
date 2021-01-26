6b04b430ccde07d5229372d12f84e78faefd6bb8
lathifrdp <lathifrdp@gmail.com>
2021-01-26 16:07:14 +0700

    kasih init fragment di main activity

--- app/src/main/java/mobile/lathifrdp/ujikomjsc/MainActivity.kt
+++ app/src/main/java/mobile/lathifrdp/ujikomjsc/MainActivity.kt
@@ -2,10 +2,21 @@ package mobile.lathifrdp.ujikomjsc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import mobile.lathifrdp.ujikomjsc.fragment.BerandaFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFragment(BerandaFragment())
+    }
+
+    protected fun initFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.screen_area, fragment)
+        fragmentTransaction.commit()
     }
 }
